6588cecc7ec9a4581217a73af559a1ec14dd5fa9
Choi Jiwon <winning1993@naver.com>
2020-06-02 18:20:05 +0900

    모달창 css, 애니메이션

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const images = document.getElementsByClassName("example_img"),
-texts = document.getElementsByClassName("example_text");
+texts = document.getElementsByClassName("example_text"),
+toggleBtn = document.getElementById("toggleBtn"),
+modal = document.getElementById("modal"),
+exitBtn = document.getElementById("exitBtn")
 
 function imgClick(e){
     const content = e.target.parentNode.childNodes[3].innerHTML;
@@ -14,10 +17,23 @@ function readText(e){
     console.log(e.target.parentNode.lastchild)
 }
 
+function showModal(){
+    modal.style.animation = "showModal 0.7s forwards";
+    toggleBtn.removeEventListener("click", showModal);
+    exitBtn.addEventListener("click", hideModal)
+}
+
+function hideModal(){
+    modal.style.animation = "hideModal 0.7s forwards";
+    exitBtn.removeEventListener("click", hideModal);
+    toggleBtn.addEventListener("click", showModal)
+}
+
 function init(){
     for(let i=0 ; i< images.length; i++){
         images[i].addEventListener("click",imgClick);
     };
+    toggleBtn.addEventListener("click", showModal)
 }
 
 init();
