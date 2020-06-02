04f214424bccb8594c33bea28cbdc9c87cbbd073
Choi Jiwon <winning1993@naver.com>
2020-06-02 23:17:15 +0900

    보이스 선택 구현

--- script.js
+++ script.js
@@ -2,12 +2,41 @@ const images = document.getElementsByClassName("example_img"),
 texts = document.getElementsByClassName("example_text"),
 toggleBtn = document.getElementById("toggleBtn"),
 modal = document.getElementById("modal"),
-exitBtn = document.getElementById("exitBtn")
+exitBtn = document.getElementById("exitBtn"),
+selectVoice = document.getElementById("selectVoice"),
+voiceOptions = document.getElementsByClassName("voice-option")
+
+let voiceIndex = 0;
+
+function makeOptions(){
+    const voices = window.speechSynthesis.getVoices();
+    for(let i=0 ; i<voices.length ; i++){
+       voiceOptions[i].innerHTML =  voices[i].name;
+       voiceOptions[i].value = i;
+    }
+}
+
+function showModal(){
+    modal.style.animation = "showModal 0.7s forwards";
+    toggleBtn.removeEventListener("click", showModal);
+    toggleBtn.addEventListener("click", hideModal);
+    exitBtn.addEventListener("click", hideModal)
+    makeOptions()
+}
+
+function hideModal(){
+    modal.style.animation = "hideModal 0.7s forwards";
+    exitBtn.removeEventListener("click", hideModal);
+    toggleBtn.addEventListener("click", showModal)
+}
 
 function imgClick(e){
     const content = e.target.parentNode.childNodes[3].innerHTML;
     console.log(content)
+    const voices = window.speechSynthesis.getVoices()
+    console.log(voices)
     const speak = new SpeechSynthesisUtterance(content);
+    speak.voice = voices[voiceIndex]
     window.speechSynthesis.speak(speak);
     e.target.style.animation = "clicked 1s";
     setTimeout(() =>{e.target.style.animation = "";},1000);
@@ -17,23 +46,17 @@ function readText(e){
     console.log(e.target.parentNode.lastchild)
 }
 
-function showModal(){
-    modal.style.animation = "showModal 0.7s forwards";
-    toggleBtn.removeEventListener("click", showModal);
-    exitBtn.addEventListener("click", hideModal)
-}
-
-function hideModal(){
-    modal.style.animation = "hideModal 0.7s forwards";
-    exitBtn.removeEventListener("click", hideModal);
-    toggleBtn.addEventListener("click", showModal)
+function changeVoice(e){
+    console.log(e.target)
+    voiceIndex = e.target.value
 }
 
 function init(){
     for(let i=0 ; i< images.length; i++){
         images[i].addEventListener("click",imgClick);
     };
-    toggleBtn.addEventListener("click", showModal)
+    toggleBtn.addEventListener("click", showModal);
+    selectVoice.addEventListener("change", changeVoice);
 }
 
 init();
